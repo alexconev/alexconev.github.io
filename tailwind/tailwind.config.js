6cc89bba116929e22419dbfdb0083e501dd25a16
Aleksandar Tsonev <alex.conev@gmail.com>
2024-12-05 14:57:27 +0200

    Update CV

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../index.html"],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#163853',
+      'secondary': '#434343',
+      'background': '#dedede',
+      'white': '#ffffff',
+    },
+  },
+  plugins: [],
+}
+
